069044f9b0b1fae6346993526726c94db828ea74
pelus131 <pelus131@hotmail.com>
2023-01-03 18:07:48 -0600

    Agregado Grid de 4x3 y funcion de agregar imagenes al grid.

--- Slideshow/Form1.cs
+++ Slideshow/Form1.cs
@@ -1,15 +1,26 @@
 using System.IO;
 using System.Drawing.Printing;
 
+
 namespace Slideshow
 {
     public partial class Form1 : Form
     {
         List<string> Imagefiles = new List<string>();
         int imageCount = 0;
+        int addcount = -1;
+        PictureBox[] pictureboxes = new PictureBox[12];
+
+
+
+
         public Form1()
         {
             InitializeComponent();
+            pictureboxes[0] = pictureBox2; pictureboxes[1] = pictureBox3; pictureboxes[2] = pictureBox4;
+            pictureboxes[3] = pictureBox5; pictureboxes[4] = pictureBox6; pictureboxes[5] = pictureBox7;
+            pictureboxes[6] = pictureBox8; pictureboxes[7] = pictureBox9; pictureboxes[8] = pictureBox10;
+            pictureboxes[9] = pictureBox11; pictureboxes[10] = pictureBox12; pictureboxes[11] = pictureBox13;
         }
 
         private void explore_Click(object sender, EventArgs e)
@@ -30,7 +41,7 @@ namespace Slideshow
             string[] files = Directory.GetFiles(path);
             foreach (string s in files)
             {
-                if (s.EndsWith(".jpg") || s.EndsWith(".jpeg")) 
+                if (s.EndsWith(".jpg") || s.EndsWith(".jpeg"))
                 {
                     Imagefiles.Add(s);
                 }
@@ -57,7 +68,7 @@ namespace Slideshow
                 imageCount += 1;
                 route.Text = nextImage;
             }
-            
+
         }
 
         private void prev_Click(object sender, EventArgs e)
@@ -79,11 +90,14 @@ namespace Slideshow
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string var=textBox1.Text;
+            string var = textBox1.Text;
 
-            var match = Imagefiles.FirstOrDefault(value => value.Contains(var));
+            var match = Imagefiles.Find(value => value.Contains(var));
+
             route.Text = match;
             pictureBox1.ImageLocation = match;
+
+
         }
 
         private void print_Click(object sender, EventArgs e)
@@ -91,12 +105,16 @@ namespace Slideshow
             PrintDialog pd = new PrintDialog();
             PrintDocument doc = new PrintDocument();
             doc.PrintPage += Doc_PrintPage;
+
             pd.Document = doc;
             if (pd.ShowDialog() == DialogResult.OK)
             {
 
                 doc.Print();
+
             }
+
+
         }
 
         private void Doc_PrintPage(object sender, PrintPageEventArgs e)
@@ -107,6 +125,87 @@ namespace Slideshow
             //****(bm,0,0)
             e.Graphics.DrawImage(pictureBox1.Image, 0, 0);
             // bm.Dispose();
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+           
+            if (addcount < 11)
+            {
+                addcount++;
+
+                /* switch (addcount)
+                 {
+                     case 2:
+                         pictureBox2.ImageLocation = route.Text;
+
+                         break;
+
+                     case 3:
+                         pictureBox3.ImageLocation = route.Text;
+                         break;
+
+                     case 4:
+                         pictureBox4.ImageLocation = route.Text;
+                         break;
+
+                     case 5:
+                         pictureBox5.ImageLocation = route.Text;
+                         break;
+
+                     case 6:
+                         pictureBox6.ImageLocation = route.Text;
+                         break;
+
+                     case 7:
+                         pictureBox7.ImageLocation = route.Text;
+                         break;
+
+                     case 8:
+                         pictureBox8.ImageLocation = route.Text;
+                         break;
+
+                     case 9:
+                         pictureBox9.ImageLocation = route.Text;
+                         break;
+                     case 10:
+                         pictureBox10.ImageLocation = route.Text;
+                         break;
+
+                     case 11:
+                         pictureBox11.ImageLocation = route.Text;
+                         break;
+
+                     case 12:
+                         pictureBox12.ImageLocation = route.Text;
+                         break;
+
+                     case 13:
+                         pictureBox13.ImageLocation = route.Text;
+                         break;
+                 }
+                */
+
+
+                pictureboxes[addcount].ImageLocation = route.Text;
+                
+
+
+
+
+
+            }
+            else { MessageBox.Show("All picture boxex  are full!"); }
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            if (addcount >= 0)
+            {
+                pictureboxes[addcount].Image = null;
+                addcount--;
+            }
+            else { MessageBox.Show("All picture boxes are empty!"); }
         }
     }
 }
